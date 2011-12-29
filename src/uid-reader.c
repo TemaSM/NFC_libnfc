446cf93c55e0fbea694e923e7a22b17e36d53d6e
Kevin Wang <kev.y.wang@gmail.com>
2011-12-29 14:02:08 -0500

    Updated uid-reader to list all passive tags, not only first

--- src/uid-reader.c
+++ src/uid-reader.c
@@ -1,5 +1,11 @@
-// Source: http://code.google.com/p/libnfc/source/browse/trunk/examples/doc/quick_start_example1.c
-// Added a few cosmetic edits.
+// Original source: http://code.google.com/p/libnfc/source/browse/trunk/examples/doc/quick_start_example1.c
+//
+// Modifications:
+//  - List all passive tags, up to 10, instead of first tag.
+//
+// Compile with C99 mode to be able to say for (int i = 0; ...). Else have to declare
+// the loop variable outside the loop.
+// c.f. http://cplusplus.syntaxerrors.info/index.php?title='for'_loop_initial_declaration_used_outside_C99_mode
 
 #ifdef HAVE_CONFIG_H
 #  include "config.h"
@@ -22,22 +28,19 @@ print_hex (const byte_t * pbtData, const size_t szBytes)
 int
 main (int argc, const char *argv[])
 {
-  nfc_device_t *pnd;
-  nfc_target_t nt;
-
   // Display libnfc version
-  const char *acLibnfcVersion = nfc_version ();
+  const char *acLibnfcVersion = nfc_version();
   printf ("%s uses libnfc %s\n", argv[0], acLibnfcVersion);
 
   // Connect using the first available NFC device
-  pnd = nfc_connect (NULL);
+  nfc_device_t * pnd = nfc_connect(NULL);
 
   if (pnd == NULL) {
     fprintf (stderr, "Unable to connect to NFC device.\n");
     return EXIT_FAILURE;
   }
   // Set connected NFC device to initiator mode
-  nfc_initiator_init (pnd);
+  nfc_initiator_init(pnd);
 
   printf ("Connected to NFC reader: %s\n", pnd->acName);
 
@@ -46,21 +49,31 @@ main (int argc, const char *argv[])
     .nmt = NMT_ISO14443A,
     .nbr = NBR_106,
   };
-  if (nfc_initiator_select_passive_target (pnd, nmMifare, NULL, 0, &nt)) {
-    printf ("The following (NFC) ISO14443A tag was found:\n");
-    printf ("    ATQA (SENS_RES): ");
-    print_hex (nt.nti.nai.abtAtqa, 2);
-    printf ("       UID (NFCID%c): ", (nt.nti.nai.abtUid[0] == 0x08 ? '3' : '1'));
-    print_hex (nt.nti.nai.abtUid, nt.nti.nai.szUidLen);
-    printf ("      SAK (SEL_RES): ");
-    print_hex (&nt.nti.nai.btSak, 1);
-    if (nt.nti.nai.szAtsLen) {
-      printf ("          ATS (ATR): ");
-      print_hex (nt.nti.nai.abtAts, nt.nti.nai.szAtsLen);
+
+  // Only list up to MAX_TARGETS targets
+  const size_t MAX_TARGETS = 10;
+  size_t numTargetsFound = 0;
+  nfc_target_t targets[MAX_TARGETS];
+  if (nfc_initiator_list_passive_targets(pnd, nmMifare, targets, MAX_TARGETS, &numTargetsFound)) {
+    for (size_t i = 0; i < numTargetsFound; ++i) {
+      nfc_target_t nt = targets[i];
+
+      printf ("The following (NFC) ISO14443A tag was found:\n");
+      printf ("    ATQA (SENS_RES): ");
+      print_hex (nt.nti.nai.abtAtqa, 2);
+      printf ("       UID (NFCID%c): ", (nt.nti.nai.abtUid[0] == 0x08 ? '3' : '1'));
+      print_hex (nt.nti.nai.abtUid, nt.nti.nai.szUidLen);
+      printf ("      SAK (SEL_RES): ");
+      print_hex (&nt.nti.nai.btSak, 1);
+      if (nt.nti.nai.szAtsLen) {
+        printf ("          ATS (ATR): ");
+        print_hex (nt.nti.nai.abtAts, nt.nti.nai.szAtsLen);
+      }
+      printf ("\n");
     }
   }
   // Disconnect from NFC device
-  nfc_disconnect (pnd);
+  nfc_disconnect(pnd);
   return EXIT_SUCCESS;
 }
 
