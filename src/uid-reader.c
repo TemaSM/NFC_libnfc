fdcaf238bd3a77b1993e06b4b28f9f31bcecd59a
Kevin Wang <kev.y.wang@gmail.com>
2011-12-29 20:30:13 -0500

    Successfully selecting particular tag

--- src/uid-reader.c
+++ src/uid-reader.c
@@ -25,6 +25,79 @@ print_hex (const byte_t * pbtData, const size_t szBytes)
   printf ("\n");
 }
 
+void 
+printISO14443ATagInfo(nfc_target_t * nt)
+{
+  printf ("The following (NFC) ISO14443A tag was found:\n");
+  printf ("    ATQA (SENS_RES): ");
+  print_hex (nt->nti.nai.abtAtqa, 2);
+  printf ("       UID (NFCID%c): ", (nt->nti.nai.abtUid[0] == 0x08 ? '3' : '1'));
+  print_hex (nt->nti.nai.abtUid, nt->nti.nai.szUidLen);
+  printf ("      SAK (SEL_RES): ");
+  print_hex (&(nt->nti.nai.btSak), 1);
+  if (nt->nti.nai.szAtsLen) {
+    printf ("          ATS (ATR): ");
+    print_hex (nt->nti.nai.abtAts, nt->nti.nai.szAtsLen);
+  }
+  printf ("\n");
+}
+
+
+bool
+selectPassiveTarget(nfc_device_t * pnd, nfc_target_t * target)
+{
+    return nfc_initiator_select_passive_target(pnd, target->nm,
+      target->nti.nai.abtUid, target->nti.nai.szUidLen, NULL);
+}
+
+// The card reader can store some auth keys. Once we store these auth keys
+// in the card reader, they can be used to authenticate with the tag being read/written.
+//
+// This function sets the given reader's key at keyNum equal to keyVal.
+bool
+loadAuthKeysIntoReader(nfc_device_t * reader, const byte_t keyNum,
+                       const byte_t * keyVal, const size_t keyValSize)
+{
+  // TODO
+  return false;
+}
+
+// Once we have auth keys saved into the reader, we need to use them
+// to auth the appropriate blocks on the tag.
+//
+// This function auths the given target tag block with the key stored at the
+// given key number of the given card.
+bool
+authenticateWithTag(nfc_device_t * reader, const byte_t keyNum, const byte_t blockNum)
+{
+  // TODO
+  return false;
+}
+
+// Once we have auth'ed the appropriate sector on the target tag, we can
+// read or write to the blocks in that sector.
+//
+// This function reads some bytes from a block on the target tag.
+bool
+readBlocks(nfc_device_t * reader, 
+           const byte_t blockNum, const size_t numBytes,
+           byte_t * response, size_t * responseSize)
+{
+  // TODO
+  return false;
+}
+           
+
+// This function writes some bytes to a block on the target tag.
+bool
+writeBlocks(nfc_device_t * reader, const byte_t blockNum,
+            const byte_t * data, const size_t dataSize)
+{
+  // TODO
+  return false;
+}
+
+
 int
 main (int argc, const char *argv[])
 {
@@ -55,21 +128,54 @@ main (int argc, const char *argv[])
   size_t numTargetsFound = 0;
   nfc_target_t targets[MAX_TARGETS];
   if (nfc_initiator_list_passive_targets(pnd, nmMifare, targets, MAX_TARGETS, &numTargetsFound)) {
-    for (size_t i = 0; i < numTargetsFound; ++i) {
-      nfc_target_t nt = targets[i];
+    printf ("[Found %zu target(s).]\n", numTargetsFound);
 
-      printf ("The following (NFC) ISO14443A tag was found:\n");
-      printf ("    ATQA (SENS_RES): ");
-      print_hex (nt.nti.nai.abtAtqa, 2);
-      printf ("       UID (NFCID%c): ", (nt.nti.nai.abtUid[0] == 0x08 ? '3' : '1'));
-      print_hex (nt.nti.nai.abtUid, nt.nti.nai.szUidLen);
-      printf ("      SAK (SEL_RES): ");
-      print_hex (&nt.nti.nai.btSak, 1);
-      if (nt.nti.nai.szAtsLen) {
-        printf ("          ATS (ATR): ");
-        print_hex (nt.nti.nai.abtAts, nt.nti.nai.szAtsLen);
+    for (size_t i = 0; i < numTargetsFound; ++i) {
+      // Select targets[i] as the target
+      nfc_target_t * nt = &targets[i];;
+      if (!selectPassiveTarget(pnd, nt)) {
+        printf ("Failed to select tag with UID: ");
+        print_hex (nt->nti.nai.abtUid, nt->nti.nai.szUidLen);
+        continue;
       }
-      printf ("\n");
+      printISO14443ATagInfo(nt);
+
+      /************************************************************************
+      // (1) Load authentication keys into the reader
+      if (!loadAuthKeysIntoReader(pnd, ...)) {
+        printf ("Failed to load auth keys into reader for target tag UID: ");
+        print_hex (nt->nti.nai.abtUid, nt->nti.nai.szUidLen);
+        continue;
+      }
+
+      // (2) Authenticate with tag using the loaded auth keys
+      if (!authenticateWithTag(pnd, ...)) {
+        printf ("Failed to authenticate with target tag UID: ");
+        print_hex (nt->nti.nai.abtUid, nt->nti.nai.szUidLen);
+        continue;
+      }
+
+      // (3) Read data from tag
+      if (!readBlocks(pnd, ...)) {
+        printf ("Failed to read blocks from target tag UID: ");
+        print_hex (nt->nti.nai.abtUid, nt->nti.nai.szUidLen);
+        continue;
+      }
+
+      // (4) Write data to tag
+      if (!writeBlocks(pnd, ...)) {
+        printf ("Failed to read blocks 2nd time from target tag UID: ");
+        print_hex (nt->nti.nai.abtUid, nt->nti.nai.szUidLen);
+        continue;
+      }
+
+      // (5) Read data from tag and confirm it was the written data
+      if (!readBlocks(pnd, ...)) {
+        printf ("Failed to read blocks 2nd time from target tag UID: ");
+        print_hex (nt->nti.nai.abtUid, nt->nti.nai.szUidLen);
+        continue;
+      }
+      ************************************************************************/
     }
   }
   // Disconnect from NFC device
